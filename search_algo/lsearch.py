a746cb9a288639eb9518bced8363f6684b6d1e0f
k1ssa1 <samikissai.edu@gmail.com>
2023-01-17 23:34:56 +0100

    Doc: adding time complexity to linear search

--- search_algo/lsearch.py
+++ search_algo/lsearch.py
@@ -1,3 +1,8 @@
+# The time complexity of this code is O(log n)
+# because the algorithm performs a single pass through the data set of size n
+# and in the worst case scenario it compares each element to the target element once
+# The time taken to perform the search increases linearly with the size of the data set
+
 def linear_search(ds,n,target):
     for i in range(0,n):
         if (ds[i] == target):
