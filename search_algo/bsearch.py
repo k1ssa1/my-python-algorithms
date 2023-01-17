b052fb3277b9d5312b3334e59097a707f684ca75
k1ssa1 <samikissai.edu@gmail.com>
2023-01-17 22:19:32 +0100

    Adding comments for the binary search algorithm

--- search_algo/bsearch.py
+++ search_algo/bsearch.py
@@ -1,15 +1,20 @@
 # This is a function that performs a binary search on a sorted list
 
 def binary_search(arr, low , high, x):
+
+    # checks if the highest value of the list is bigger than it's lowest
     if high > low:
         mid = (high + low) // 2
 
+        # checks if the target value x is equal to the list middle value
         if arr[mid] == x:
             return mid
 
+        # if the middle value is greater than the target value we only focus on the left half of the list
         elif arr[mid] > x:
             return binary_search(arr ,low, mid - 1, x)
         
+        # if the middle value is lower than x we focus on the right half of the list
         else:
             return binary_search(arr, mid + 1, high, x)
 
--- search_algo/output.py
+++ search_algo/output.py
@@ -1,10 +1,14 @@
+# module imports
 import bsearch as bs
 
+# defining the list  and the target
 arr = [1,3,6,9,12,18]
 x = 12
 
+# function calling
 result = bs.binary_search(arr, 0, len(arr) - 1, x)
 
+# test message
 if result != -1:
     print("Element is present at index", str(result))
 else:
